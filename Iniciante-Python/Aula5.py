61810eacdbf8885207d675fa4a2b59a6404f452f
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-02-25 19:56:33 -0300

    Aula 5 referente ao tipo boolean

---
+++ Iniciante-Python/Aula5.py
@@ -0,0 +1,14 @@
+#Tipo de dado bool (boolean)
+#Ao questionar algo em um programa,
+#So existem duas respostas possíveis;
+#sim (true) ou não(false)
+# Existem vários operadores para "questionar"
+#Dentre eles os ==, que é um operador lógico que
+#questiona se um valor é igual a outro
+
+print(10 == 10) #True => sim (verdadeiro)
+print(10 == 11) #False => não (false)
+print(type(True))
+print(type(False))
+print(type(10 == 10)) 
+print(type(10 == 11)) 
