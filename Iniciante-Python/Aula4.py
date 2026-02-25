2c8ec65c2668a352495eb4998a7588f26bd9be08
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-02-25 19:55:10 -0300

    Modificado

--- Iniciante-Python/Aula4.py
+++ Iniciante-Python/Aula4.py
@@ -3,12 +3,27 @@
 # O tipo int representa qualquer número
 # positivo ou negativo . int sem sinal é considerado
 # positivo.
+"""
+print(11) #int
+print(-11) #int
+print(0) #int
+"""
 
 
 # Float -> Número com ponto flutuante
 # O tipo float representa qualquer npumero
 #positivo ou negativo com ponto flutuante 
 # Float sem sinal é consideradp positivo.
+"""
+print(1.1, 10.11 ) #float - para separa numero com casas decimais use . ponto
+print(0.0) #float
+print(-1.5) # float
+"""
+
+
 
 # A função type mostra o tipo que o Python
 #inferiu ao valor.
+print(    type('Nayara')     ) # se tem letra minsculas e parentes  função ou classe(object) - type() fala qual o tipo 
+print(    type(-1)     )
+print(type(0.1), type(-1.1),type(0.0))
