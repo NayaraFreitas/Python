226e164bec2f11d405079273201599eac835f035
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-02-28 19:44:50 -0300

    Aula 8, exercicio referente a aula 7 sobre variaveis

---
+++ Iniciante-Python/Aula8-exercicio_aula7.py
@@ -0,0 +1,13 @@
+nome = 'Eliza'
+sobrenome = 'Fernanades'
+idade = 28
+ano_nascimento = 2026 - idade
+maior_de_idade = idade >= 18
+aultura_metros = 1.72
+
+print('Nome:', nome)
+print('Sobrenome:',sobrenome)
+print('Idade:' , idade)
+print('Ano de nascimento:' , ano_nascimento)
+print('É maior de idade:', maior_de_idade)
+print('Altura em metros:' , aultura_metros)
