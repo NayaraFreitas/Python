87f01707de55a022a008844f0520f7551c5f1adb
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-01 19:27:38 -0300

    Aula 13 : f string (formatação de string)

---
+++ Iniciante-Python/Aula13-exercicio copy.py
@@ -0,0 +1,11 @@
+nome = 'Nayara Freitas'
+altura = 1.70
+peso = 86
+imc = peso / (altura * altura)
+print('Nome:', nome , 'Sua altura:', altura , 'Seu peso:',peso , 'Seu IMC é:' , imc)
+
+linha_1 = f'Seu nome:{nome},  Sua altura é:{altura:.2f},  Seu peso:{peso},  Seu imc é:{imc} ' #formatação de string
+
+print(linha_1)
+print(f'Seu imc:{imc}')
+
