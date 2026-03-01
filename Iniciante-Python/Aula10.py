1ddaa3b2aee3541408984435b97c3b072d36a486
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-01 17:06:19 -0300

    Aula 10 : concatenação(+) e repetição(*)

---
+++ Iniciante-Python/Aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'A' + 'B' + 'C'
+print(concatenacao) # utilizando o + para concatenarou sej ajuntar as palavras
+
+a_dez_vezes = 'A' * 10  # usando a multiplicação pode repitir  uma str 
+tres_vezes_luiz = 3 * 'Luiz'
+print(a_dez_vezes)
+print(tres_vezes_luiz)
