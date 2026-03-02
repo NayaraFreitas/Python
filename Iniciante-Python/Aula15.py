fe8fb4a741f7639ba88d3fb814355a1052a76d09
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-02 14:42:51 -0300

    Aula 15: função input

---
+++ Iniciante-Python/Aula15.py
@@ -0,0 +1,19 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 = input('Digite um numero:')
+numero_2 = input('Digite  outro numero:')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos numeros é : {int_numero_1 + int_numero_2}')
+
+
+# Tentando fazer a lógica , mas falhei - só teste para quando for digitado str 
+"""if numero_1 == str(numero_1):
+    print('Por favor digite apenas números')
+elif numero_2 == str(numero_2):
+    print('Por favor digite apenas números')
+else :
+    print(f'A soma dos número é {int(numero_1) + int(numero_2)}')"""
