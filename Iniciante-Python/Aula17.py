92c396f36246db47b5e06c9c8a2e487e07569d36
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-04 21:54:05 -0300

    Aula 17: review sobre if, elif , else

---
+++ Iniciante-Python/Aula17.py
@@ -0,0 +1,24 @@
+# Aula de review de if elif e else
+# condicao = True
+condicao1 = True
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+
+if condicao1:
+    print('Este é o código de if')
+elif condicao2:
+    print('condição 2')
+elif condicao3:
+    print('condiçaõ 3')
+elif condicao4:
+     pass #existe esse pass para quando precisar passar , ou não tem codigo criado ou cedrto
+else:
+    print('Este é o else do primeiro if')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Foara do if')
+
