95230f7ff8845e594017cadb8ddb710da3816498
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-07 22:11:18 -0300

    Aula 23 : operador lógico NOT

---
+++ Iniciante-Python/Aula23.py
@@ -0,0 +1,12 @@
+# Operador lógico NOT
+# usado para inverter expressões
+# not True = False
+# not False  = True
+"""
+senha = input('Senha: ')
+
+if not senha :
+  print('Não digitou nada')"""
+
+print(not True) #False
+print(not False) #True
