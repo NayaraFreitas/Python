39a7e6926484f1e08711d2070f354b546b61bfbc
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-13 19:34:04 -0300

    Aula 35: continuação While

---
+++ Iniciante-Python/Aula35.py
@@ -0,0 +1,15 @@
+"""
+Repetições
+While (enquanto)
+Executa uma ação enquanto uma condição for verdadeira entra do while
+Loop infinito -> Quando um código não tem fim ou seja , se uma condicao for true e não tiver o break para parar 
+"""
+
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
+
