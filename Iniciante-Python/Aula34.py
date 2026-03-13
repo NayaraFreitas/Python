d4966a08a4ab8fde3b17b912d0392eae5d17d182
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-13 18:12:27 -0300

    Aula 34: Laço de repetição While

---
+++ Iniciante-Python/Aula34.py
@@ -0,0 +1,25 @@
+"""
+Repetições
+While (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim ou seja , se uma condicao for true e não tiver o break para parar 
+"""
+condicao = True
+
+while condicao:
+    nome = input('Digite seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome ==  'Sair':
+        break
+
+
+# while condicao:
+#     print(1)
+#     print(2)
+#     print(3)
+#     break
+    
+
+
+
