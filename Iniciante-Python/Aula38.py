707d8e089eed73ef9f1132394a5794d72dc4736b
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-03-13 22:36:36 -0300

    Aula 38 :While + While

---
+++ Iniciante-Python/Aula38.py
@@ -0,0 +1,22 @@
+"""
+While + While
+"""
+
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+
+while linha <= qtd_linhas:
+    coluna = 1
+    print(f' espçao entre as linhas e colunas ={linha}')
+
+    while coluna <= qtd_colunas:
+        print(f'{linha=}, {coluna=}')
+        coluna += 1
+
+
+    linha += 1
+
+print('Acabou')
