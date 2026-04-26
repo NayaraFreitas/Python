78c80b1897bc9b76640ea4b849a89b5f41c2e04a
NayaraFreitas <nnayarafreitasteles14@gmail.com>
2026-04-26 19:53:28 -0300

    Aula 44: Range e For in

---
+++ Iniciante-Python/Aula44.py
@@ -0,0 +1,14 @@
+"""
+For + Range
+range -> range(start, stop , step)
+"""
+
+#numeros = range(10) # quer dizer numero de 0 á 10
+#numeros = range(5, 10) # aqui será de 5 á 10 
+numeros = range(0, 88, 8) # aqui vai de 5 á 10 pulando de 2 em 2
+
+#print(numeros) # aqui so mostra o range para ve os numero sendo mostrado tem que usar o for
+
+#aqui ira mostrar o numeros do range
+for valor in numeros:
+    print(valor)
